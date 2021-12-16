1499723a3217a0bab84791d0971ef1552cf5bc22
manoel1845 <manoel_carlos16@hotmail.com>
2021-12-16 23:59:24 +0000

    add sgs

--- k8s/terraform/sg.tf
+++ k8s/terraform/sg.tf
@@ -1,71 +1,71 @@
-# resource "aws_security_group" "acessos_masters" {
-#   name        = "k8s-acessos_masters-${var.meu_nome}"
-#   description = "acessos inbound traffic"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "acessos_masters" {
+  name        = "k8s-acessos_masters-${var.project_name}"
+  description = "acessos inbound traffic"
+  vpc_id = aws_vpc.main.id
 
-#   egress = [
-#     {
-#       from_port        = 0
-#       to_port          = 0
-#       protocol         = "-1"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       ipv6_cidr_blocks = [],
-#       prefix_list_ids = null,
-#       security_groups: null,
-#       self: null,
-#       description: "Libera dados da rede interna"
-#     }
-#   ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
 
-#   tags = {
-#     Name = "acessos_masters-${var.meu_nome}"
-#   }
-# }
+  tags = {
+    Name = "acessos_masters-${var.project_name}"
+  }
+}
 
-# resource "aws_security_group" "acessos_haproxy" {
-#   name        = "k8s-haproxy-${var.meu_nome}"
-#   description = "acessos inbound traffic"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "acessos_haproxy" {
+  name        = "k8s-haproxy-${var.project_name}"
+  description = "acessos inbound traffic"
+  vpc_id = aws_vpc.main.id
 
-#   egress = [
-#     {
-#       from_port        = 0
-#       to_port          = 0
-#       protocol         = "-1"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       ipv6_cidr_blocks = [],
-#       prefix_list_ids = null,
-#       security_groups: null,
-#       self: null,
-#       description: "Libera dados da rede interna"
-#     }
-#   ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
 
-#   tags = {
-#     Name = "allow_haproxy_ssh-${var.meu_nome}"
-#   }
-# }
+  tags = {
+    Name = "allow_haproxy_ssh-${var.project_name}"
+  }
+}
 
-# resource "aws_security_group" "acessos_workers" {
-#   name        = "k8s-workers-${var.meu_nome}"
-#   description = "acessos inbound traffic"
-#   vpc_id = var.vpc_id
+resource "aws_security_group" "acessos_workers" {
+  name        = "k8s-workers-${var.project_name}"
+  description = "acessos inbound traffic"
+  vpc_id = aws_vpc.main.id
 
-#   egress = [
-#     {
-#       from_port        = 0
-#       to_port          = 0
-#       protocol         = "-1"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#       ipv6_cidr_blocks = [],
-#       prefix_list_ids = null,
-#       security_groups: null,
-#       self: null,
-#       description: "Libera dados da rede interna"
-#     }
-#   ]
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
 
-#   tags = {
-#     Name = "acessos_workers-${var.meu_nome}"
-#   }
-# }
+  tags = {
+    Name = "acessos_workers-${var.project_name}"
+  }
+}

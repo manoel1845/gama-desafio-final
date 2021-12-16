1e839f590d9226293d9de684dd5665ba4643a0e7
manoel1845 <manoel_carlos16@hotmail.com>
2021-12-16 23:50:20 +0000

    add vpc terraform file

--- k8s/terraform/variables.tf
+++ k8s/terraform/variables.tf
@@ -1,16 +1,16 @@
-variable project_name {
+variable "project_name" {
   type        = string
   default     = "turma3"
   description = "Adicionar seu nome"
 }
 
-variable region {
+variable "region" {
   type        = string
   default     = "sa-east-1"
   description = "Região da AWS"
 }
 
-variable vpc_cidr_block {
+variable "vpc_cidr_block" {
   type        = string
   default     = "10.10.0.0/16"
   description = "VPC CIDR block"

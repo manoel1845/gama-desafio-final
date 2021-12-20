46fedffbde74010df949723e26a8d02a61854ad5
mendcav <casarica2007@gmail.com>
2021-12-20 00:58:05 +0000

    add java dev deploy

--- 3-java_dev/buildImage.sh
+++ 3-java_dev/buildImage.sh
@@ -3,7 +3,7 @@
 cd app-code
 export DOCKER_USERNAME="manoel1845"
 export DOCKER_PASSWORD="GamaDesafio123"
-docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker build -t manoel1845/javaspring:0.0.1 -f Dockerfile .
-docker tag manoel1845/javaspring:0.0.1 hub.docker.com/r/manoel1845/javaspring:0.0.1
-docker push manoel1845/javaspring:0.0.1
+sudo docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+sudo docker build -t manoel1845/javaspring:0.0.1 -f Dockerfile .
+sudo docker tag manoel1845/javaspring:0.0.1 hub.docker.com/r/manoel1845/javaspring:0.0.1
+sudo docker push manoel1845/javaspring:0.0.1

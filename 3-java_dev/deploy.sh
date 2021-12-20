730e51f03b7627c6f578c671ec0428f057a13bf0
mendcav <casarica2007@gmail.com>
2021-12-20 01:19:52 +0000

    fix deploy.sh

--- 3-java_dev/deploy.sh
+++ 3-java_dev/deploy.sh
@@ -3,4 +3,4 @@
 cd 3-java_dev/ansible
 
 echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/kp-mineiro.pem ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarDev.yaml -u ubuntu --private-key /var/lib/jenkins/kp-mineiro.pem
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yaml -u ubuntu --private-key /var/lib/jenkins/kp-mineiro.pem

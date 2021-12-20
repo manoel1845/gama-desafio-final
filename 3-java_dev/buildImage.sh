3da0e8b016f17cd188f0608c250d3fc0c66720c2
mendcav <casarica2007@gmail.com>
2021-12-20 01:06:33 +0000

    add java dev deploy

--- 3-java_dev/buildImage.sh
+++ 3-java_dev/buildImage.sh
@@ -4,6 +4,12 @@ cd app-code
 export DOCKER_USERNAME="manoel1845"
 export DOCKER_PASSWORD="GamaDesafio123"
 sudo docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+echo "
+FROM openjdk:8-jdk-alpine
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
+" > Dockerfile
 sudo docker build -t manoel1845/javaspring:0.0.1 -f Dockerfile .
 sudo docker tag manoel1845/javaspring:0.0.1 hub.docker.com/r/manoel1845/javaspring:0.0.1
 sudo docker push manoel1845/javaspring:0.0.1
